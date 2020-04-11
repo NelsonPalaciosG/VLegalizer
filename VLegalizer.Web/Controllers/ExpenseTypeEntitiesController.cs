f5e13b3538f9a5df22a6adf78f09f447d656efb9
NELSON ALEJANDRO PALACIOS GALEANO <nelsonpalacios98055@correo.itm.edu.co>
2020-04-11 01:25:17 -0500

    Add ExpenseType controller, TripEntity controller, improve this with javascript

---
+++ VLegalizer.Web/Controllers/ExpenseTypeEntitiesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VLegalizer.Web.Data;
+using VLegalizer.Web.Data.Entities;
+
+namespace VLegalizer.Web.Controllers
+{
+    public class ExpenseTypeEntitiesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ExpenseTypeEntitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ExpenseTypeEntities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ExpenseTypes.ToListAsync());
+        }
+
+        // GET: ExpenseTypeEntities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseTypeEntity = await _context.ExpenseTypes.FindAsync(id);
+            if (expenseTypeEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseTypeEntity);
+        }
+
+        // GET: ExpenseTypeEntities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ExpenseTypeEntity expenseTypeEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                expenseTypeEntity.ExpenseNames = expenseTypeEntity.ExpenseNames.ToUpper();
+                _context.Add(expenseTypeEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(expenseTypeEntity);
+        }
+
+        // GET: ExpenseTypeEntities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseTypeEntity = await _context.ExpenseTypes.FindAsync(id);
+            if (expenseTypeEntity == null)
+            {
+                return NotFound();
+            }
+            return View(expenseTypeEntity);
+        }
+
+        // POST: ExpenseTypeEntities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ExpenseTypeEntity expenseTypeEntity)
+        {
+            if (id != expenseTypeEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                expenseTypeEntity.ExpenseNames = expenseTypeEntity.ExpenseNames.ToUpper();
+                _context.Update(expenseTypeEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseTypeEntity);
+        }
+
+        // GET: ExpenseTypeEntities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseTypeEntity = await _context.ExpenseTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expenseTypeEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.ExpenseTypes.Remove(expenseTypeEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ VLegalizer.Web/Controllers/TripEntitiesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VLegalizer.Web.Data;
+using VLegalizer.Web.Data.Entities;
+
+namespace VLegalizer.Web.Controllers
+{
+    public class TripEntitiesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TripEntitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TripEntities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Trips.ToListAsync());
+        }
+
+        // GET: TripEntities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(tripEntity);
+        }
+
+        // GET: TripEntities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TripEntities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StartDate,EndDate,TotalAmount,City")] TripEntity tripEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tripEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tripEntity);
+        }
+
+        // GET: TripEntities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips.FindAsync(id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+            return View(tripEntity);
+        }
+
+        // POST: TripEntities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StartDate,EndDate,TotalAmount,City")] TripEntity tripEntity)
+        {
+            if (id != tripEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tripEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripEntityExists(tripEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tripEntity);
+        }
+
+        // GET: TripEntities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(tripEntity);
+        }
+
+        // POST: TripEntities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tripEntity = await _context.Trips.FindAsync(id);
+            _context.Trips.Remove(tripEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripEntityExists(int id)
+        {
+            return _context.Trips.Any(e => e.Id == id);
+        }
+    }
+}
